f6374a6b5e467ea9a7a6371fdbb09bb52e0c3c0d
Matteo Tomasini <matteo.tomasini@dataexpert.it>
2024-02-28 15:45:13 +0100

    Commented native Android notification.

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.kt
@@ -19,29 +19,12 @@ class GeofencingBroadcastReceiver : BroadcastReceiver() {
     private val TAG = "GeoBroadcastReceiver"
 
     override fun onReceive(context: Context, intent: Intent) {
-        val geofencingEvent = GeofencingEvent.fromIntent(intent) ?: return
+        //val geofencingEvent = GeofencingEvent.fromIntent(intent) ?: return
 
         Log.v(TAG, context.getString(R.string.geofence_triggered))
-//        val fenceId = when {
-//            geofencingEvent.triggeringGeofences?.isNotEmpty() == true ->
-//                geofencingEvent.triggeringGeofences?.get(0)?.requestId
-//
-//            else -> {
-//                Log.e(TAG, "No Geofence Trigger Found! Abort mission!")
-//                return
-//            }
-//        }
-//        val foundIndex = GeofencingConstants.LANDMARK_DATA.indexOfFirst {
-//            it.id == fenceId
-//        }
-//
-//        if (foundIndex == -1) {
-//            Log.e(TAG, "Unknown Geofence: Abort Mission")
-//            return
-//        }
 
         // Get the local notification manager.
-        val notificationManager = ContextCompat.getSystemService(
+        /*val notificationManager = ContextCompat.getSystemService(
                 context,
                 NotificationManager::class.java
             ) as NotificationManager
@@ -56,7 +39,7 @@ class GeofencingBroadcastReceiver : BroadcastReceiver() {
             notificationManager.sendGeofenceExitedNotification(
                 context, 0
             )
-        }
+        }*/
         FlutterMain.startInitialization(context)
         FlutterMain.ensureInitializationComplete(context, null)
 
